ac81fc0a34f8044b8d67285c7481b0da157e8d60
jmarsic <jmarsovac@gmail.com>
2023-06-25 17:24:52 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const cars = [
+  {
+    Marka: "Fiat",
+    Model: "Punto",
+    GodinaProizvodnje: 1998,
+    Boja: "Plava",
+    KonskaSnaga: 55,
+  },
+  {
+    Marka: "Ford",
+    Model: "Fiesta",
+    GodinaProizvodnje: 2014,
+    Boja: "Crna",
+    KonskaSnaga: 95,
+  },
+  {
+    Marka: "Renault",
+    Model: "Megan",
+    GodinaProizvodnje: 2008,
+    Boja: "Crna",
+    KonskaSnaga: 112,
+  },
+  {
+    Marka: "Skoda",
+    Model: "Fabia",
+    GodinaProizvodnje: 2016,
+    Boja: "Plava",
+    KonskaSnaga: 95,
+  },
+  {
+    Marka: "Renault",
+    Model: "4",
+    GodinaProizvodnje: 1987,
+    Boja: "Bijela",
+    KonskaSnaga: 34,
+  },
+  {
+    Marka: "Volkswagen",
+    Model: "Golf GTI",
+    GodinaProizvodnje: 1990,
+    Boja: "Crvena",
+    KonskaSnaga: 110,
+  },
+];
+
+console.log(cars);
