db238b02e16923071c04064ac8c7057e0116a385
jmarsic <jmarsovac@gmail.com>
2023-06-28 23:09:10 +0200

    update cars array and add horsepower functions

--- index.js
+++ index.js
@@ -1,46 +1,128 @@
 const cars = [
   {
-    Marka: "Fiat",
-    Model: "Punto",
-    GodinaProizvodnje: 1998,
-    Boja: "Plava",
-    KonskaSnaga: 55,
+    manufacturer: "Fiat",
+    model: "Punto",
+    yearOfManufacture: 1998,
+    countryOfOrigin: "Italija",
+    color: "Plava",
+    horsepower: 55,
   },
   {
-    Marka: "Ford",
-    Model: "Fiesta",
-    GodinaProizvodnje: 2014,
-    Boja: "Crna",
-    KonskaSnaga: 95,
+    manufacturer: "Ford",
+    model: "Fiesta",
+    yearOfManufacture: 2014,
+    countryOfOrigin: "Amerika",
+    color: "Crna",
+    horsepower: 95,
   },
   {
-    Marka: "Renault",
-    Model: "Megan",
-    GodinaProizvodnje: 2008,
-    Boja: "Crna",
-    KonskaSnaga: 112,
+    manufacturer: "Renault",
+    model: "Megan",
+    yearOfManufacture: 2008,
+    countryOfOrigin: "Francuska",
+    color: "Crna",
+    horsepower: 112,
   },
   {
-    Marka: "Skoda",
-    Model: "Fabia",
-    GodinaProizvodnje: 2016,
-    Boja: "Plava",
-    KonskaSnaga: 95,
+    manufacturer: "Skoda",
+    model: "Fabia",
+    yearOfManufacture: 2016,
+    countryOfOrigin: "Ceska",
+    color: "Plava",
+    horsepower: 95,
   },
   {
-    Marka: "Renault",
-    Model: "4",
-    GodinaProizvodnje: 1987,
-    Boja: "Bijela",
-    KonskaSnaga: 34,
+    manufacturer: "Renault",
+    model: "4",
+    yearOfManufacture: 1987,
+    countryOfOrigin: "Francuska",
+    color: "Bijela",
+    horsepower: 34,
   },
   {
-    Marka: "Volkswagen",
-    Model: "Golf GTI",
-    GodinaProizvodnje: 1990,
-    Boja: "Crvena",
-    KonskaSnaga: 110,
+    manufacturer: "Volkswagen",
+    model: "Golf GTI",
+    yearOfManufacture: 1990,
+    countryOfOrigin: "Njemacka",
+    color: "Crvena",
+    horsepower: 110,
   },
 ];
 
-console.log(cars);
+// console.log(cars);
+
+// for (car of cars) {
+//   console.log(car.color);
+// }
+
+//1. sortorat niz auta po godinama, ispisat novi niz
+
+function sortCarsByYear() {
+  const carsByYear = cars.sort((car) => {
+    car.yearOfManufacture;
+  });
+  return carsByYear;
+}
+console.log(sortCarsByYear());
+// const years = cars.map((car) => {
+//   return car.yearOfManufacture;
+// });
+
+// const result = years.sort((a, b) => {
+//   return a - b;
+// });
+
+//2. pronac prvi automobil odgovarajuce boje, ispisat cijeli objekt auta
+//4. novi niz koji sadrzava automobile samo odredene boje, ispisat novi niz
+//5. pretty print
+//6. isprintat aute koji su prozvedeni izmedju 2 poslana parametra godina, ispisat novi niz
+//7. dodavanje novog automobila preko prompta
+
+//8. ispisat prosjecnu KS i auto cija KS najvise odskace od prosjecne
+function printMaxHp() {
+  const average = averageHorsepower();
+  let maxHp = 0;
+  let carWithMaxHp;
+
+  for (let car of cars) {
+    if (car.horsepower > maxHp) {
+      maxHp = car.horsepower;
+    }
+  }
+  for (let car in cars) {
+    carWithMaxHp = cars.find((car) => {
+      return car.horsepower === maxHp;
+    });
+  }
+  console.log(average, carWithMaxHp);
+}
+
+printMaxHp();
+
+//9. napravit novi niz ferarija, formatirat tako da ubacimo povlake
+
+// const printCars = [`Marka: fiat`, `Marka: golf`];
+// console.log(printCars);
+
+//3. provjerit postoji li auto koji ima vise od prosjecne KS
+function averageHorsepower() {
+  let average = 0;
+  let sum = 0;
+
+  for (let car of cars) {
+    sum += car.horsepower;
+  }
+  average = sum / cars.length;
+
+  return average;
+}
+
+function isHpBiggerThenAverage() {
+  const average = averageHorsepower();
+  for (car of cars) {
+    if (car.horsepower > average) return true;
+  }
+  return false;
+}
+
+console.log(isHpBiggerThenAverage());
